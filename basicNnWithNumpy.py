db7466362041aa8c786e9114c90da1b81a57d861
karaatf <atfkara19@gmail.com>
2024-07-03 19:57:55 +0300

    first code

--- basicNnWithNumpy.py
+++ basicNnWithNumpy.py
@@ -12,14 +12,20 @@ class LayerDense:
         self.weights = 0.10 * np.random.randn(n_inputs, n_neuron)
         self.biases = np.zeros((1, n_neuron))
 
-
     def forward_prop(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
 class ReLU:
     def forward(self, inputs):
-        self.output = np.maximum(0,inputs)
+        self.output = np.maximum(0, inputs)
+
+
+class Softmax:
+
+    def forward(self,inputs):
+        exp_values = np.exp(inputs-np.max(inputs, axis=1, keepdims=True))
+        self.output = exp_values/np.sum(exp_values, axis=1, keepdims=True)
 
 
 layer1 = LayerDense(2,5)
@@ -27,3 +33,6 @@ layer1.forward_prop(X)
 relu = ReLU()
 relu.forward(layer1.output)
 print(relu.output)
+softmax = Softmax()
+softmax.forward(layer1.output)
+print(softmax.output)
